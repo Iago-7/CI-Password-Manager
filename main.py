a06e5b565e5036628be3c5f8e7f7c82e9d3206e1
Christopher McLean <71855991+Iago-7@users.noreply.github.com>
2023-09-16 13:42:01 -0700

    Cleaned up positioning of widgets on screen.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 FONT_NAME = "Arial"
-FONT_SIZE = 36
+FONT_SIZE = 20
 
 
 def generate_password():
@@ -10,7 +10,7 @@ def generate_password():
 
 window = Tk()
 window.title("CI Password Manager")
-window.config(padx=100, pady=20, bg="white")
+window.config(padx=20, pady=20, bg="white")
 
 canvas = Canvas(width=202, height=320, highlightthickness=0, highlightbackground="white")
 logo_img = PhotoImage(file="CI-PW-Logo.png")
@@ -18,28 +18,28 @@ canvas.create_image(101, 160, image=logo_img)
 canvas.grid(row=0, column=1)
 
 website_label = Label(text="Website:", bg="white", font=(FONT_NAME, FONT_SIZE))
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky=E)
 
 username_label = Label(text="Email/Username:", bg="white", font=(FONT_NAME, FONT_SIZE))
-username_label.grid(row=2, column=0)
+username_label.grid(row=2, column=0, sticky=E)
 
 password_label = Label(text="Password:", bg="white", font=(FONT_NAME, FONT_SIZE))
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky=E)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry()
+website_entry.grid(row=1, column=1, columnspan=2, sticky=EW)
 
-username_entry = Entry(width=35)
-username_entry.grid(row=2, column=1, columnspan=2)
+username_entry = Entry()
+username_entry.grid(row=2, column=1, columnspan=2, sticky=EW)
 
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry = Entry(width=33)
+password_entry.grid(row=3, column=1, sticky=EW)
 
 generate_password_button = Button(text="Generate Password", highlightthickness=0, highlightbackground="white",
                                   command=generate_password)
-generate_password_button.grid(row=3, column=3)
+generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=36, highlightthickness=0, highlightbackground="white")
-add_button.grid(row=4, column=1, columnspan=2)
+add_button = Button(text="Add", highlightthickness=0, highlightbackground="white")
+add_button.grid(row=4, column=1, columnspan=2, sticky=EW)
 
 window.mainloop()
