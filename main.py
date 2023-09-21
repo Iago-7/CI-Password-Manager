254e98d87c34dba7efd1eb3b2dc42d590d960c1d
Christopher McLean <71855991+Iago-7@users.noreply.github.com>
2023-09-20 18:24:38 -0700

    Added functionality for generating a random secure password.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 
 
 FONT_NAME = "Arial"
@@ -7,7 +9,25 @@ FONT_SIZE = 20
 
 
 def generate_password():
-    password_entry.insert(END, string="TEST")
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    pw_letter_list = [choice(letters) for _ in range(randint(8, 12))]
+    pw_symbol_list = [choice(symbols) for _ in range(randint(2, 4))]
+    pw_number_list = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_char_list = pw_letter_list + pw_symbol_list + pw_number_list
+
+    shuffle(password_char_list)
+
+    password = "".join(password_char_list)
+
+    password_entry.delete(0, END)
+    password_entry.insert(0, string=password)
+    pyperclip.copy(password)
 
 
 def save_login_info():
